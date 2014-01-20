31a2cab7c51f4e5fb254786c6d2e36014436e063
Sebastian Kent <sebastian.kent@memcf.co.uk>
2014-01-20 08:18:11 +0000

    fixed nhibernate shizzems

---
+++ LoanBook/Services/Domain/LoanPaymentService.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+
+namespace Services.Domain
+{
+    public class LoanPaymentService
+    {
+        public void MakePayment(Payment payment, Loan loan)
+        {
+            
+        }
+    }
+}
